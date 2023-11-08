3204e5579c1f4fcadb5822b03cc0912e58727d39
Ley Low <chuenleylow@gmail.com>
2023-11-09 03:05:13 +1100

    debug

--- src/pages/PracticeTranslation.svelte
+++ src/pages/PracticeTranslation.svelte
@@ -32,37 +32,37 @@
         });
 
         try {
-            console.log(tokenizer.tokenize("彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた"))
-            data = await getTranslationQuestion();
-            // data = {
-            //     id: "123123123123123",
-            //     uid: "",
-            //     user: null,
-            //     sentence_pairs: [
-            //         {
-            //             id: "12345",
-            //             question: "彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた。彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた。彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた。",
-            //             answer: "",
-            //             solution: "",
-            //             score: 0,
-            //         },
-            //         {
-            //             id: "123456",
-            //             question: "この小説は直訳すると意味がよくわからないが、読み手が感じたことを大切にすれば、非常に面白い作品であるといえる。",
-            //             answer: "",
-            //             solution: "",
-            //             score: 0,
-            //         },
-            //         {
-            //             id: "1234567",
-            //             question: "私が知りたいことは、あなたがその問題を解決するために何を考えているか、そして具体的にどのような行動を取るつもりなのかです。",
-            //             answer: "",
-            //             solution: "",
-            //             score: 0,
-            //         }
-            //     ],
-            //     language: "Japanese",
-            // }
+            //console.log(tokenizer.tokenize("彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた"))
+            //data = await getTranslationQuestion();
+            data = {
+                id: "123123123123123",
+                uid: "",
+                user: null,
+                sentence_pairs: [
+                    {
+                        id: "12345",
+                        question: "彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた。彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた。彼が部屋に入ってきた時、私はテレビを見ていたわけではなく、ラジオを聴いていた。",
+                        answer: "",
+                        solution: "",
+                        score: 0,
+                    },
+                    {
+                        id: "123456",
+                        question: "この小説は直訳すると意味がよくわからないが、読み手が感じたことを大切にすれば、非常に面白い作品であるといえる。",
+                        answer: "",
+                        solution: "",
+                        score: 0,
+                    },
+                    {
+                        id: "1234567",
+                        question: "私が知りたいことは、あなたがその問題を解決するために何を考えているか、そして具体的にどのような行動を取るつもりなのかです。",
+                        answer: "",
+                        solution: "",
+                        score: 0,
+                    }
+                ],
+                language: "Japanese",
+            }
             let tokenized_sentence_pairs = []
             for (let pair of data.sentence_pairs) {
                 let tokenized_question = tokenizer.tokenize(pair.question);
