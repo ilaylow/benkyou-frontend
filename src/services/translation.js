dd81cd2ab34fb8dce1cc3239e6780299690fbaa8
Ley Low <chuenleylow@gmail.com>
2023-11-09 03:44:33 +1100

    fixed typo

--- src/services/translation.js
+++ src/services/translation.js
@@ -35,6 +35,7 @@ const getTranslationQuestion = async () => {
 };
 
 const getTranslationMark = async (translations) => {
+    // Remove tokenized questions before sending to server for marking
     let sendTranslations = structuredClone(translations);
     for (let pair of sendTranslations.sentence_pairs) {
         delete pair.tokenized_question;
@@ -46,10 +47,14 @@ const getTranslationMark = async (translations) => {
 
     const response = await fetch(markQuestionUrl, getMarkRequestOptions(translations, setHeaders(jwt)));
     let markedTranslations = await response.json();
+
+    // Attach back tokenized questions when marked translations are received
     for (let pair of markedTranslations.sentence_pairs) {
         const matchPair = translations.sentence_pairs.find(p => p.id == pair.id)
         pair.tokenized_question = matchPair.tokenized_question
     }
+
+    return markedTranslations
 
 }
 
